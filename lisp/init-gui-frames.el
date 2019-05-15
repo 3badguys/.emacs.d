520860e9d13d1fc468773fa72e28a154b273ef7e
ACT3 <chuiC456@163.com>
2019-05-15 10:26:51 -0400

    Change some gui features.

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -1,14 +1,27 @@
 ;;; init-gui-frames.el --- Behaviour specific to non-TTY frames
 
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-(when (fboundp 'set-scroll-bar-mode)
-  (set-scroll-bar-mode -1))
-
+;; suppress GUI features
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
 (setq inhibit-splash-screen t)
 
-(setq-default cursor-type 'box)  ;; bar or box
+;; show a marker in the left fringe for lines not in the buffer
+(setq indicate-empty-lines t)
 
+;; full screen after init emacs
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; no tool bar
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+;; no scroll bar
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode -1))
+;; no menu bar
+(when (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
+
+;; set cursor type, bar or box
+(setq-default cursor-type 'box)
 
 (provide 'init-gui-frames)
