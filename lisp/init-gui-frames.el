bfdf2e3fd8d4623299cd1a3bd0c11efde773560e
ACT3 <chuiC456@163.com>
2019-05-22 13:42:30 -0400

    Close scroll-bar-mode.

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -17,8 +17,8 @@
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 ;; no scroll bar
-(when (fboundp 'set-scroll-bar-mode)
-  (set-scroll-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
 ;; no menu bar
 (when (fboundp 'menu-bar-mode)
   (menu-bar-mode -1))
