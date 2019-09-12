02a15f95e799e1c813511351bd06edc75cb7e9c9
ACT3 <chuiC456@163.com>
2019-09-13 07:46:49 +0800

    Use C-n C-p as company's select shortcut.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -2,4 +2,11 @@
 
 (add-hook 'after-init-hook 'global-company-mode)
 
+;; Use C-n C-p to select
+(after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-company)
