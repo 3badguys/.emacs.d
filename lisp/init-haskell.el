88a3d379b0e987b678c9a45f0ebcab0c17d2cf6b
ACT3 <chuiC456@163.com>
2019-10-13 21:13:50 +0800

    Update haskell-mode config.

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,9 +1,11 @@
 ;;; init-haskell.el --- Config for haskell
 
 (require 'haskell-mode)
+(require 'haskell-indentation)
 
 (add-auto-mode 'haskell-mode "\\.ghci\\'")
 
+(add-hook 'haskell-mode-hook 'turn-on-font-lock)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
