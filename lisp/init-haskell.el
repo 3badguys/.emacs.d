ef45752c396f7e196fce78fa57e2afc148b9a274
ACT3 <chuiC456@163.com>
2019-10-18 21:11:07 +0800

    Update init-haskell.

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -5,8 +5,14 @@
 
 (add-auto-mode 'haskell-mode "\\.ghci\\'")
 
+(add-hook 'haskell-mode-hook 'subword-mode)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (add-hook 'haskell-mode-hook 'turn-on-font-lock)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
+
+(after-load 'haskell-mode
+  (define-key haskell-mode-map (kbd "C-c h") 'hoogle)) ;; hoogle is a haskell API search engine
 
 (provide 'init-haskell)
