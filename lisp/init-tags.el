6e196ecda746b6c85f655a0a8a72ab3aa44ee9c8
ACT3 <chuiC456@163.com>
2019-10-18 21:47:16 +0800

    Separate create-tags into async and sync.

--- lisp/init-tags.el
+++ lisp/init-tags.el
@@ -1,13 +1,25 @@
 ;;; init-tags.el --- Config for tags
 
-;; Create TAGS file through ctags
-(defun create-tags (dir-name)
-  "Create tags file."
+;; Create TAGS file through ctags asyn.
+(defun create-tags-async (dir-name)
+  "Create tags file asyn."
   (interactive "DTAG-Root: ")
   (start-process-shell-command
    ""
    nil
    (format "ctags -f TAGS -e -R %s" (directory-file-name dir-name))))
+
+;; Create TAGS file through ctags sync.
+;; In order to debug when creating tags file failed.
+(defun create-tags-sync (dir-name)
+  "Create tags file sync."
+  (interactive "DTAG-Root: ")
+  (shell-command
+   (format "ctags -f TAGS -e -R %s" (directory-file-name dir-name))))
+
+;; Define keybindings for create-tags-funcs.
+(global-set-key (kbd "C-c c a") 'create-tags-async)
+(global-set-key (kbd "C-c c s") 'create-tags-sync)
 
 ;; ggtags
 ;; (add-hook 'c-mode-common-hook
