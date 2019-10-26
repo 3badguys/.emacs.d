0bdbfa6b38055fead1f4499b0ffb90e8f8fdaa3b
ACT3 <chuiC456@163.com>
2019-10-26 21:52:48 +0800

    Config whitespace-mode.

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -34,4 +34,20 @@
 (setq display-time-day-and-date t)
 (display-time-mode) ; show date in modeline
 
+;; whitespace-mode
+(progn
+  ;; Make whitespace-mode with very basic background coloring for whitespaces.
+  ;; http://ergoemacs.org/emacs/whitespace-mode.html
+  (setq whitespace-style (quote (face spaces tabs newline space-mark tab-mark newline-mark )))
+
+  ;; Make whitespace-mode and whitespace-newline-mode use “¶” for end of line char and “▷” for tab.
+  (setq whitespace-display-mappings
+        ;; all numbers are unicode codepoint in decimal. e.g. (insert-char 182 1)
+        '(
+          (space-mark 32 [183] [46]) ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+          (newline-mark 10 [182 10]) ; LINE FEED,
+          (tab-mark 9 [9655 9] [92 9]) ; tab
+          ))
+  )
+
 (provide 'init-gui-frames)
