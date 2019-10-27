1a991758db1a476544f6c1ae5c51bf2de78ac2c9
ACT3 <chuiC456@163.com>
2019-10-27 11:48:47 +0800

    Fix undo-tree's keybindings can't work.

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -6,6 +6,11 @@
 (when (executable-find "aspell")
   (setq-default ispell-program-name "aspell")
   ;; Add spell-checking in comments for all programming language modes
-  (add-hook 'prog-mode-hook 'flyspell-prog-mode))
+  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+
+  (after-load 'flyspell
+    (define-key flyspell-mode-map (kbd "C-;") nil)
+    (define-key flyspell-mode-map (kbd "C-,") nil)
+    (define-key flyspell-mode-map (kbd "C-.") nil)))
 
 (provide 'init-spelling)
