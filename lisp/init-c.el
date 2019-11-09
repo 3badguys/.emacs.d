a4d635d785417eecf20bf10d2b4de81031f44706
ACT3 <chuiC456@163.com>
2019-11-10 02:16:11 +0800

    Update init-c.

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,6 +1,24 @@
-;;; init-c.el --- Config for c
+;;; init-c.el --- Configurations for C programming language.
 
-(setq-default c-basic-offset 4)
-(setq c-default-style "linux")
+(defun 3badguys-c-mode-common-defaults ()
+  (setq c-default-style "k&r"
+        c-basic-offset 4)
+  (c-set-offset 'substatement-open 0))
+
+(setq 3badguys-c-mode-common-hook '3badguys-c-mode-common-defaults)
+
+;; this will affect all modes derived from cc-mode, like
+;; java-mode, php-mode, etc
+(add-hook 'c-mode-common-hook (lambda ()
+                                (run-hooks '3badguys-c-mode-common-hook)))
+
+(defun 3badguys-makefile-mode-defaults ()
+  (whitespace-toggle-options '(tabs))
+  (setq indent-tabs-mode t))
+
+(setq 3badguys-makefile-mode-hook '3badguys-makefile-mode-defaults)
+
+(add-hook 'makefile-mode-hook (lambda ()
+                                (run-hooks '3badguys-makefile-mode-hook)))
 
 (provide 'init-c)
