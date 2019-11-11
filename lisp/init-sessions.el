0ae548aa242572fae5357f84f8af68cdc50989b7
ACT3 <chuiC456@163.com>
2019-11-12 01:42:07 +0800

    Update desktop-buffers-not-to-save && desktop-modes-not-to-save.

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -6,4 +6,15 @@
 (require 'desktop)
 (desktop-save-mode t)
 
+;; Copy from https://www.emacswiki.org/emacs/Desktop
+(setq desktop-buffers-not-to-save
+      (concat "\\("
+              "^nn\\.a[0-9]+\\|\\.log\\|(ftp)\\|^tags\\|^TAGS"
+              "\\|\\.emacs.*\\|\\.diary\\|\\.newsrc-dribble\\|\\.bbdb"
+              "\\)$"))
+(add-to-list 'desktop-modes-not-to-save 'dired-mode)
+(add-to-list 'desktop-modes-not-to-save 'Info-mode)
+(add-to-list 'desktop-modes-not-to-save 'info-lookup-mode)
+(add-to-list 'desktop-modes-not-to-save 'fundamental-mode)
+
 (provide 'init-sessions)
