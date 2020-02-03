c339d5c647cd585f861a22dd052ab2a32efca869
ACT3 <chuiC456@163.com>
2020-02-03 16:52:37 +0800

    Disable changes to ctrl&meta; Use Esc do Ctrl+g.

--- lisp/init-xah-fly-keys.el
+++ lisp/init-xah-fly-keys.el
@@ -1,5 +1,9 @@
 ;;; init-xah-fly-keys.el --- Config for xah-fly-keys
 
+;; must come before loading xah-fly-keys
+(setq xah-fly-use-control-key nil)
+(setq xah-fly-use-meta-key nil)
+
 (require 'xah-fly-keys)
 
 (xah-fly-keys-set-layout "qwerty") ; required
@@ -19,5 +23,8 @@
 ;; "norman"
 
 (xah-fly-keys 1)
+
+;; Make Escape key do emacs's Ctrl+g
+(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
 
 (provide 'init-xah-fly-keys)
