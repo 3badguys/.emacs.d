c0875f8f1c4bfa237c5c2d6855665153e0bfb1fe
ACT3 <chuiC456@163.com>
2020-02-06 04:36:29 +0800

    Update init-abbrev.el.

--- lisp/init-abbrev.el
+++ lisp/init-abbrev.el
@@ -1,9 +1,36 @@
 ;;; init-abbrev.el --- Config for abbrev mode
 
+;; Note:
+;;      1. Press Ctrl+q before typing space or punctuation, can stop abbrev from expansion.
+;;      2. M+x list-abbrevs → Display a list of defined abbrevs.
+
+(clear-abbrev-table global-abbrev-table)
+(define-abbrev-table 'global-abbrev-table
+  '(
+    ("3bg" "3badguys")
+
+    ;; english word abbrev
+    ("u" "you")
+    ("r" "are")
+    ("ur" "you are")
+    ("bg" "background")
+    ("thx" "thanks")
+    ("cnt" "can't")
+    ("dnt" "don't")
+    ("wnt" "won't")
+
+    ;; computing
+    ("cs" "computer science")
+
+    ;; programming
+    ("db" "databases")
+
+    ;;
+    ))
+
+;; turn on abbrev mode globally
 (setq-default abbrev-mode t)
-(define-abbrev-table 'global-abbrev-table '(
-					                        ;; signature
-					                        ("SHIT" "T_ACT3_T")
-					                        ))
+;; won't save to the file named abbrev_defs
+(setq save-abbrevs nil)
 
 (provide 'init-abbrev)
