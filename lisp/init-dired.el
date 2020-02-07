8f22ee375a43ef309f4dacdd896b342c75e414e9
ACT3 <chuiC456@163.com>
2020-02-08 02:39:14 +0800

    Update dired config and readme content.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,16 +1,20 @@
 ;;; init-dired.el --- Dired customisations
 
+(require 'dired-x)
+
+;; always allow dired to delete or copy dir
 (setq dired-recursive-deletes 'always)
 (setq dired-recursive-copies 'always)
 
-(put 'dired-find-alternate-file 'disabled nil)
-
-(require 'dired-x)
-
+;; let the other dir in the split pane to be default destination
 (setq dired-dwim-target t)
 
+;; http://ergoemacs.org/emacs/emacs_dired_tips.html
+;; open the file/directory without creating a new buffer
+(put 'dired-find-alternate-file 'disabled nil)
 (after-load 'dired
-  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+  (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))))
 
 ;; TODO: what's the target of dwin?
 ;; dwin = do what i mean.
