9409b3c91871ce73a973b551c04dc1da3c85f10b
ACT3 <chuiC456@163.com>
2020-02-13 23:59:40 +0800

    Remove unused functions.

--- lisp/back/init-folding.el
+++
@@ -1,9 +0,0 @@
-;;; init-folding.el --- Config for folding.
-
-;; Origami, a text folding minor mode
-(add-hook 'prog-mode-hook 'origami-mode)
-(with-eval-after-load 'origami
-  (define-key origami-mode-map (kbd "C-c f") 'origami-recursively-toggle-node)
-  (define-key origami-mode-map (kbd "C-c F") 'origami-toggle-all-nodes))
-
-(provide 'init-folding)
--- lisp/init-edit-utils.el
+++ lisp/init-edit-utils.el
@@ -27,56 +27,6 @@
 ;; auto revert
 (global-auto-revert-mode)
 
-;; indent the text content
-(defun indent-buffer()
-  (interactive)
-  (indent-region (point-min) (point-max)))
-
-(defun indent-region-or-buffer()
-  (interactive)
-  (save-excursion
-    (if (region-active-p)
-	    (progn
-	      (indent-region (region-beginning) (region-end))
-	      (message "Indent selected region."))
-      (progn
-	    (indent-buffer)
-	    (message "Indent buffer.")))))
-
-(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
-
-;; handle eol
-(defun hidden-dos-eol ()
-  "Do not show ^M in files containing mixed UNIX and DOS line endings."
-  (interactive)
-  (unless buffer-display-table
-    (setq buffer-display-table (make-display-table)))
-  (aset buffer-display-table ?\^M []))
-
-(defun remove-dos-eol ()
-  "Replace DOS eolns CR LF with Unix eolns CR"
-  (interactive)
-  (goto-char (point-min))
-  (while (search-forward "\r" nil t) (replace-match "")))
-
-;; comment like Eclipse
-(defun comment-eclipse ()
-  (interactive)
-  (let ((start (line-beginning-position))
-        (end (line-end-position)))
-    (when (or (not transient-mark-mode) (region-active-p))
-      (setq start (save-excursion
-                    (goto-char (region-beginning))
-                    (beginning-of-line)
-                    (point))
-            end (save-excursion
-                  (goto-char (region-end))
-                  (end-of-line)
-                  (point))))
-    (comment-or-uncomment-region start end)))
-
-(global-set-key (kbd "C-c C-c") 'comment-eclipse)
-
 ;; Copy from: https://www.emacswiki.org/emacs/SmoothScrolling
 ;; scroll one line at a time (less "jumpy" than defaults)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
