27a5237bd57551aa9d2aaede202b3c3a8f3b7294
ACT3 <chuiC456@163.com>
2020-02-14 16:51:10 +0800

    Update init-highlight.

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -1,9 +1,5 @@
 ;;; init-highlight.el --- Initialize highlight configurations.
 
-;; Highlight the current line
-(when (display-graphic-p)
-  (global-hl-line-mode t))
-
 ;; symbol-overlay mode for highlighting the symbol
 (require 'symbol-overlay)
 (dolist (hook '(prog-mode-hook html-mode-hook yaml-mode-hook conf-mode-hook))
@@ -28,17 +24,6 @@
       (highlight-indent-guides--highlighter-default level responsive display)))
   (setq highlight-indent-guides-highlighter-function
         #'3badguys-indent-guides-for-all-but-first-column)
-
-  ;; Disable `highlight-indent-guides-mode' in `swiper'
-  ;; https://github.com/DarthFennec/highlight-indent-guides/issues/40
-  (after-load 'ivy
-    (defadvice ivy-cleanup-string (after 3badguys-ivy-cleanup-hig activate)
-      (let ((pos 0) (next 0) (limit (length str)) (prop 'highlight-indent-guides-prop))
-        (while (and pos next)
-          (setq next (text-property-not-all pos limit prop nil str))
-          (when next
-            (setq pos (text-property-any next limit prop nil str))
-            (remove-text-properties next pos '(display nil face nil) str))))))
   )
 
 ;; highlight-numbers
