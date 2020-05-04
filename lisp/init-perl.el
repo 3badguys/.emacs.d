e7fd8de66fb35d3aa96039a4e4040e09f6426a9b
ACT3 <chuiC456@163.com>
2020-05-04 23:00:17 +0800

    Use PerlStyle in cperl-mode.

--- lisp/init-perl.el
+++ lisp/init-perl.el
@@ -7,10 +7,6 @@
 ;; Change default indentations
 (add-hook 'cperl-mode-hook 'n-cperl-mode-hook t)
 (defun n-cperl-mode-hook ()
-  (setq cperl-indent-level 4)
-  (setq cperl-continued-statement-offset 0)
-  (setq cperl-extra-newline-before-brace t)
-  (set-face-background 'cperl-array-face "wheat")
-  (set-face-background 'cperl-hash-face "wheat"))
+  (cperl-set-style "PerlStyle"))
 
 (provide 'init-perl)
