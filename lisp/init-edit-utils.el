6dd109afe0df4ad088f85001de9c22a8814750d1
ACT3 <chuiC456@163.com>
2020-09-06 22:22:10 +0800

    Delete smooth scrolling feature.

--- lisp/init-edit-utils.el
+++ lisp/init-edit-utils.el
@@ -39,13 +39,6 @@
 ;; auto revert
 (global-auto-revert-mode)
 
-;; Copy from: https://www.emacswiki.org/emacs/SmoothScrolling
-;; scroll one line at a time (less "jumpy" than defaults)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
-;; (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-(setq scroll-step 1) ;; keyboard scroll one line at a time
-
 (progn
   ;; Don't disable case-change functions
   (put 'upcase-region 'disabled nil)   ;; C-x C-u
