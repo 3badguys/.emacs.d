1e228549b5b6a0a450ccc2204b1df039d76a3b6c
ACT3 <chuiC456@163.com>
2023-12-07 01:18:02 +0800

    Remove ctags program for windows-nt.

--- lisp/init-tags.el
+++ lisp/init-tags.el
@@ -5,10 +5,5 @@
 (setq large-file-warning-threshold nil)
 
 (require 'tbg-jump)
-;; In windows system, use the ctags program in ~/.emacs.d/bin/ctags/ctags.exe
-(when (string-equal system-type "windows-nt")
-  (setq tbg-jump-ctags-program-path
-        (file-truename (concat user-emacs-directory
-                               "bin/ctags/ctags.exe"))))
 
 (provide 'init-tags)
