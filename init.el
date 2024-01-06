eb99d356d25e99ae8bd8a77cacb2ce8f85abca75
ACT3 <chuiC456@163.com>
2024-01-06 22:46:07 +0800

    Remove some useless config.

--- init.el
+++ init.el
@@ -26,47 +26,32 @@
     (normal-top-level-add-subdirs-to-load-path)))
 (add-subdirs-to-load-path (expand-file-name "site-lisp" user-emacs-directory))
 
+;; Bootstrap config
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
-;; Normally file-name-handler-alist is set to
-;; (("\\`/[^/]*\\'" . tramp-completion-file-name-handler)
-;; ("\\`/[^/|:][^/|]*:" . tramp-file-name-handler)
-;; ("\\`/:" . file-name-non-special))
-;; Which means on every .el and .elc file loaded during start up, it has to runs those regexps against the filename.
-(let* ((file-name-handler-alist nil))
-  (require 'init-utils)
-  ;; Call (package-initialize)
-  (require 'init-elpa)      ;; Install required packages
+(require 'init-utils)
+(require 'init-elpa)
 
-  ;; Allow users to provide an optional "init-preload-local.el"
-  (require 'init-preload-local nil t)
+(require 'init-xah-fly-keys)
+(require 'init-gui-frames)
+(require 'init-sessions)
+(require 'init-edit-utils)
+(require 'init-search)
+(require 'init-abbrev)
+(require 'init-hippie-expand)
+(require 'init-dired)
+(require 'init-tags)
+(require 'init-org)
+(require 'init-lisp)
 
-  ;; Load configs for specific features and modes
-  (require 'init-xah-fly-keys)
-  (require 'init-gui-frames)
-  (require 'init-sessions)
-  (require 'init-edit-utils)
-  (require 'init-search)
-  (require 'init-abbrev)
-  (require 'init-hippie-expand)
-  (require 'init-dired)
-  (require 'init-tags)
-  (require 'init-org)
-  (require 'init-lisp)
+;; Variables configured via the interactive 'customize' interface
+(load (expand-file-name "custom.el" user-emacs-directory) "NOERROR" "NOMESSAGE")
 
-  ;; Variables configured via the interactive 'customize' interface
-  (load (setq custom-file (expand-file-name "custom.el" user-emacs-directory)) t t)
-
-  ;; Allow users to provide an option "init-local" containing presonal settings
-  (require 'init-local nil t)
-
-  ;; Load personal tbg-xxx packages
-  (require 'tbg-cloc)
-  (require 'tbg-find-replace)
-  (require 'tbg-header)
-  ;;
-  )
+;; Load personal tbg-xxx packages
+(require 'tbg-cloc)
+(require 'tbg-find-replace)
+(require 'tbg-header)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
