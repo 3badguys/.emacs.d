6045da71c38dc93ba668ef2e0c4e9ccb5e8ead06
ACT3 <chuiC456@163.com>
2024-02-12 13:02:19 +0800

    Init org mode through use-package.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,36 +1,31 @@
 ;;; init-org.el --- Org-mode config
 
-(require 'org)
+(use-package org
+  :ensure nil
+  :bind (("C-c a"  . org-agenda))
+  :config
+  (setq
+   ;; when opening a org file, don't collapse headings
+   org-startup-folded nil
+   ;; indent the org file
+   org-startup-indented t
+   ;; wrap long lines. don't let it disappear to the right
+   org-startup-truncated nil
+   ;; when in a url link, enter key should open it
+   org-return-follows-link t
+   ;; make org-mode” syntax color embedded source code
+   org-src-fontify-natively t
+   )
 
-;; org-mode setup
-(progn
-  ;; when opening a org file, don't collapse headings
-  (setq org-startup-folded nil)
-  ;; indent the org file
-  (setq org-startup-indented t)
-  ;; wrap long lines. don't let it disappear to the right
-  (setq org-startup-truncated nil)
-  ;; when in a url link, enter key should open it
-  (setq org-return-follows-link t)
-  ;; make org-mode” syntax color embedded source code
-  (setq org-src-fontify-natively t))
-
-(setq org-agenda-files '("~/org"))
-(global-set-key (kbd "C-c a") 'org-agenda)
-
-(setq org-ditaa-jar-path "/usr/bin/ditaa")
-
-(after-load 'org
   (org-babel-do-load-languages
    'org-babel-load-languages
    `((C . t)
-     (org . t)
      (emacs-lisp . t)
-     (latex . t)
      (python . t)
-     (,(if (locate-library "ob-sh") 'sh 'shell) . t)
+     (perl . t)
+     (shell . t)
+     (js . t)
      (lisp . t)
-     (ditaa . t)
-     (plantuml . t))))
+     (latex . t))))
 
 (provide 'init-org)
